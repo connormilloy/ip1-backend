8ad49441a9ba80d1974b6fbb6fcc3839e1a132dd
Connor <connorking.milloy@gmail.com>
2022-08-06 18:42:45 +0100

    Cors

--- Server/Server.js
+++ Server/Server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 // Import the accounts routes and expose them to the server file
@@ -9,6 +10,7 @@ const sqlite3 = require('sqlite3').verbose();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors());
 
 // Tell the express application to associate all accounts routes with the url prefix 'accounts'
 // For account creation, the URL is now '/accounts/new-account'
